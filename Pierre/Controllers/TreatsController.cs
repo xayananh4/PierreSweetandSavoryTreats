bdab792eae4ecc46f2d14a4af9d7056b457089b9
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-03-24 16:49:51 -0700

    add create crud to treats controller

--- Pierre/Controllers/TreatsController.cs
+++ Pierre/Controllers/TreatsController.cs
@@ -1,14 +1,17 @@
-using Pierre.Models;
 using Microsoft.AspNetCore.Mvc;
+using Pierre.Models;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Pierre.Controllers
 {
   public class TreatsController : Controller
   {
-    private readonly TreatContext _db;
+    private readonly PierreContext _db;
 
-    public TreatsController(TreatContext db)
+    public TreatsController(PierreContext db)
     {
       _db = db;
     }
@@ -16,5 +19,36 @@ namespace Pierre.Controllers
     {
       return View(_db.Treats.ToList());
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Treat treat)
+    {
+      if (!ModelState.IsValid)
+      {
+        ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "TreatName");
+        return View(treat);
+      }
+      else
+      {
+        _db.Treats.Add(treat);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+
+
+
+
+
+
+
+
+
   }
 }
